3c05931f4f6bec89cd39f009b577e2f0fa8f5f3e
vanessa clavijo <vanessa.cl@altitum.com>
2022-02-05 23:29:17 -0500

    CORRECCIONES

--- App.py
+++ App.py
@@ -13,7 +13,8 @@ login_manager=LoginManager()
 
 Session(app)
 app.config['SQLALCHEMY_DATABASE_URI']='postgres://yfhnvhwqisrefo:8a016092f45a3ec9bcab80e8b19fdcb9e58eb6f3146382593970b45acd83f19f@ec2-35-173-83-57.compute-1.amazonaws.com:5432/d5s4c0lso3brg0'
-
+#mysql+pymysql://root:admin@localhost/mvc
+#postgres://yfhnvhwqisrefo:8a016092f45a3ec9bcab80e8b19fdcb9e58eb6f3146382593970b45acd83f19f@ec2-35-173-83-57.compute-1.amazonaws.com:5432/d5s4c0lso3brg0
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
 
@@ -22,7 +23,7 @@ from controllers.infoController import*
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 if __name__=='__main__':
-    app.run(debug=True, host= '192.168.100.83',port=220)
+    app.run(debug=True, host= '192.168.1.18',port=220)
     app.secret_key = '684651454165416'
     app.config['SESSION_TYPE'] = 'filesystem'
 
